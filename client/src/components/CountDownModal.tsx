cb48a701e0b8f62d7c0d5cbade1b8a9f393e0003
Sabrina Zheng <szheng@Sabrinas-MacBook-Pro.local>
2024-02-04 13:51:53 -0500

    add timer modal

--- client/src/components/CountDownModal.tsx
+++ client/src/components/CountDownModal.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function CountDownModal() {
+  const [counter, setCounter] = useState(3);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (counter > 0) {
+      const timer = setTimeout(() => setCounter(counter - 1), 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setShow(false); // Hide the modal when the countdown is complete
+    }
+  }, [counter]); // Run effect whenever counter changes
+
+  //   useEffect(() => {
+  //     if (!Connecting) {
+  //       setShow(false); // Hide the modal when connecting is done
+  //     }
+  //   }, [Connecting]); // Run effect whenever connecting changes
   return (
     <div
-      className="modal fade"
+      className="Modal"
       id="countdownModal"
       tabIndex={-1}
       aria-labelledby="CountdownModalLabel"
@@ -15,7 +34,7 @@ export default function CountDownModal() {
             <h5 className="modal-title">Game Found</h5>
           </div>
           <div className="modal-body d-grid justify-content-center">
-            <div className="mt-3">__Count Down__</div>
+            <div className="mt-3">{counter}</div>
           </div>
         </div>
       </div>
