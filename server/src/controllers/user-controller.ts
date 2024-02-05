c3a8b34e1da6199532d63f3d05463843fba10630
KangQiChen <kangqi.chen@stonybrook.edu>
2024-02-05 10:43:26 -0500

    added query by wins and query by games played

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,16 +1,169 @@
-import { RequestHandler } from "express";
+import { RequestHandler, response } from "express";
 import UserModel from '../models/UserSchema';
 
+// returns all users
 export const getUsers: RequestHandler = async(request, response, next) =>{
 	try {
 		const users = await UserModel.find().exec();
-		response.status(200).json(users);
+        if (users){
+            response.status(200).json(users);
+            console.log(users);
+        }else{
+            console.log('no user within the last hour');
+        }
+		
 
 	} catch (error) {
 		next(error);
 	}
 };
 
+// // returns all users sorted by games won
+// export const getUsersByWins: RequestHandler = async(request, response, next) =>{
+// 	try {
+// 		const users = await UserModel.find()
+//         .sort(
+//             { 'matches.$[elem].won': -1 }    
+//         )
+//         .exec();
+
+//         if (users){
+//             response.status(200).json(users);
+//             console.log(users);
+//         }else{
+//             console.log('no user within the last hour');
+//         }
+
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// };
+
+// returns all users sorted by games won, include wonMatches as a field in response
+export const getUsersByWins: RequestHandler = async(request, response, next) =>{
+	try {
+		const users = await UserModel.aggregate([
+            {
+              $addFields:{
+                wonmatches:{
+                    $size:{
+                        $filter:{
+                            input: 'matches',
+                            as: 'match',
+                            cond: {$eq: ['$$match.won', true]},
+                        },
+                    },
+                },  
+              },  
+            },
+            {
+                $sort: {
+                    wonMatches: -1
+                },
+            },
+            {
+                $project:{
+                    name:1,
+                    matches:1,
+                    wonMatches:1,
+                },
+            },
+        ])
+        .exec();
+
+        if (users){
+            response.status(200).json(users);
+            console.log(users);
+        }else{
+            console.log('no user within the last hour');
+        }
+
+	} catch (error) {
+		next(error);
+	}
+};
+
+// // returns all users sorted by games played
+// export const getUsersByGamesPlayed: RequestHandler = async(request, response, next) =>{
+// 	try {
+// 		const users = await UserModel.find()
+//         .sort(
+//             { 'matches.length': -1 }    
+//         )
+//         .exec();
+
+//         if (users){
+//             response.status(200).json(users);
+//             console.log(users);
+//         }else{
+//             console.log('no user within the last hour');
+//         }
+
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// };
+
+
+// returns all users sorted by games played, includes number of matches in response
+export const getUsersByGamesPlayed: RequestHandler = async(request, response, next) =>{
+	try {
+		const users = await UserModel.aggregate([
+            {
+                $addFields:{
+                    totalMatches: {$size: '$matches'},
+                },
+            },
+            {
+                $sort:{
+                    totalMatches: -1
+                },
+            },
+            {
+                $project:{
+                    name:1,
+                    matches:1,
+                    totalMatches:1,
+                },
+            },
+        ])
+        .exec();
+
+        if (users){
+            response.status(200).json(users);
+            console.log(users);
+        }else{
+            console.log('no user within the last hour');
+        }
+
+	} catch (error) {
+		next(error);
+	}
+};
+
+// returns all user with matches within the last hour
+export const getLastHour: RequestHandler = async(request, response, next) =>{
+    try {
+        var oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
+		const users = await UserModel.find({
+            matches: {
+                $elemMatch: { timePlayed: { $gte: oneHourAgo } },
+              },
+        }).exec();
+
+        if (users) {
+            response.status(200).json(users);
+            console.log(users);
+        } else {
+            console.log('no user within the last hour');
+        }
+		
+	} catch (error) {
+		next(error);
+	}
+};
+
+// returns a user based on id
 export const getUser: RequestHandler = async(request, response, next) =>{
     const userId = request.params.userId;
 
@@ -23,6 +176,7 @@ export const getUser: RequestHandler = async(request, response, next) =>{
 	}
 };
 
+// creates a new user based on request body's name
 export const creatUser: RequestHandler = async(request, response, next) =>{
     const name = request.body.name;
     console.log("request body:" + JSON.stringify(request.body));
@@ -38,4 +192,9 @@ export const creatUser: RequestHandler = async(request, response, next) =>{
     } catch (error) {
         next(error);
     }
+};
+
+export const updateUser: RequestHandler = async(request, response, next) =>{
+
+
 };
--- server/src/routes/user-router.ts
+++ server/src/routes/user-router.ts
@@ -4,7 +4,11 @@ import * as UserController from '../controllers/user-controller'
 const router = express.Router();
 
 router.get('/', UserController.getUsers);
+router.get('/wins', UserController.getUsersByWins);
+router.get('/gamesPlayed', UserController.getUsersByGamesPlayed);
+router.get('/lasthour', UserController.getLastHour);
 router.get('/:userId', UserController.getUser);
 router.post('/', UserController.creatUser);
+router.patch('/:userId', UserController.updateUser);
 
 export default router;
