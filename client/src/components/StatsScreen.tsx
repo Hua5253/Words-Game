d9efa7b3b212fb538c02d7eadca66685c2f879cf
sdsdc1234 <sdsdc1234>
2024-02-07 10:28:42 +0800

    stats page connect backend, and add back button

--- client/src/components/StatsScreen.tsx
+++ client/src/components/StatsScreen.tsx
@@ -1,25 +1,10 @@
 import { useEffect, useState } from "react";
 import "../CSS/StatsScreen.css";
+import { useNavigate } from "react-router-dom";
 
 function StatsScreen() {
-    const mockData = [
-        { user: "jack", totalGames: 40, winGames: 10, loseGames: 30, averageTurns: 5 },
-        { user: "Mark", totalGames: 30, winGames: 20, loseGames: 10, averageTurns: 4 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-        { user: "Tom", totalGames: 20, winGames: 5, loseGames: 15, averageTurns: 3 },
-    ];
-
     const [currentTab, setCurrentTab] = useState("all");
+    const navigate = useNavigate();
 
     const [data, setData] = useState([] as any[]);
 
@@ -27,41 +12,51 @@ function StatsScreen() {
 
     useEffect(() => {
         getData();
-    }, [sortBy]);
+    }, [sortBy, currentTab]);
 
-    const getData = () => {
-        // switch (sortBy) {
-        //     case "1":
-        //         setData(mockData.sort((a, b) => b.winGames - a.winGames));
-        //         break;
-        //     case "2":
-        //         setData(mockData.sort((a, b) => a.averageTurns - b.averageTurns));
-        //         break;
-        //     case "3":
-        //         setData(mockData.sort((a, b) => b.totalGames - a.totalGames));
-        //         break;
-        //     default:
-        //         setData(mockData);
-        //         break;
-        // }
+    const getData = async () => {
+        let tempData: any[] = [];
 
-        fetch("http://127.0.0.1:3000/api/")
-            .then((res) => res.json())
-            .then((res) => {
-                console.log(res);
-            });
+        if (currentTab == "all") {
+            tempData = await fetch("http://127.0.0.1:3000/api/users").then((res) => res.json());
+        } else if (currentTab == "last") {
+            tempData = await fetch("http://127.0.0.1:3000/api/lasthour").then((res) => res.json());
+        }
+
+        switch (sortBy) {
+            case "1":
+                setData(tempData.sort((a, b) => b.matchesWon - a.matchesWon));
+                break;
+            case "2":
+                setData(tempData.sort((a, b) => a.avgTurns - b.avgTurns));
+                break;
+            case "3":
+                setData(tempData.sort((a, b) => b.totalMatches - a.totalMatches));
+                break;
+            default:
+                setData(tempData);
+                break;
+        }
     };
 
     const handleChangeTab = (tab: string) => {
-        console.log(tab);
-
         setCurrentTab(tab);
     };
 
     return (
         <>
             <div className="statsscreen">
-                <h2 className="mb-5 text-center">Leaderboard</h2>
+                <div className="position-relative">
+                    <h2 className="mb-5 text-center">Leaderboard</h2>
+                    <button
+                        type="button"
+                        className="btn btn-outline-primary position-absolute top-0"
+                        onClick={() => {
+                            navigate("/");
+                        }}>
+                        Back
+                    </button>
+                </div>
                 <div className="mb-3 d-flex gap-3">
                     <ul className="nav nav-tabs">
                         <li
@@ -95,7 +90,7 @@ function StatsScreen() {
                     </div>
                 </div>
                 <div className="table-container">
-                    <table className="table table-striped">
+                    <table className="table">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
@@ -107,14 +102,14 @@ function StatsScreen() {
                         </thead>
                         <tbody>
                             {data.map((item, index) => (
-                                <tr className={`${item.user == "jack" ? "table-primary" : ""}`} key={item.user}>
+                                <tr className={`${item._id == "jack" ? "table-primary" : ""}`} key={item._id}>
                                     <th scope="row">{index + 1}</th>
-                                    <td>{item.user}</td>
-                                    <td>{item.totalGames}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.totalMatches}</td>
                                     <td>
-                                        {item.winGames}/{item.loseGames}
+                                        {item.matchesWon}/{item.totalMatches - item.matchesWon}
                                     </td>
-                                    <td>{item.averageTurns}</td>
+                                    <td>{item.avgTurns}</td>
                                 </tr>
                             ))}
                         </tbody>
